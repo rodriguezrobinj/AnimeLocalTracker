29993d955ce220d9efce3e21ebad8c96f670ddeb
Robin Junior Rodriguez Henao <rodriguezrobinj@outlook.com>
2026-08-25 19:13:02 -0500

    Fix CS0234 namespace ambiguity for TappedEventArgs

--- AnimeLocalTracker.Avalonia/AnimeLocalTracker.Avalonia/Views/ReproductorView.axaml.cs
+++ AnimeLocalTracker.Avalonia/AnimeLocalTracker.Avalonia/Views/ReproductorView.axaml.cs
@@ -9,7 +9,7 @@ public partial class ReproductorView : UserControl
         InitializeComponent();
     }
 
-    private void VideoViewControl_OnDoubleTapped(object? sender, Avalonia.Input.TappedEventArgs e)
+    private void VideoViewControl_OnDoubleTapped(object? sender, global::Avalonia.Input.TappedEventArgs e)
     {
         e.Handled = true;
     }
